cacc026434484801f90f91995bf5026dd13b5b0d
marianoqnk <marianoqnk@gmail.com>
2018-08-28 12:30:09 +0200

    Este es el inicio

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(DD1,INPUT_PULLUP)
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
